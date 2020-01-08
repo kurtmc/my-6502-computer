2c7c99ab7a65c79b99eb1adb12f9aeb55a0ad3d7
Kurt McAlpine <kurt@linux.com>
2020-01-08 20:12:27 +1300

    Update program

--- rom-generator/main.go
+++ rom-generator/main.go
@@ -10,8 +10,22 @@ func main() {
 		rom[i] = 0xea
 	}
 
+	// 6502 reads the program start location from 0xfffc and 0xfffd, I have
+	// hooked A16 of the 6502 to chip enable on the ROM so that's my it's
+	// inserted into the rom at 0x7ffc and 0x7ffd
+	// so the program start address of 0x8000 will correspond to 0x0000 in
+	// the ROM
 	rom[0x7ffc] = 0x00
 	rom[0x7ffd] = 0x80
+
+	// lda 0x42
+	rom[0] = 0xa9
+	rom[1] = 0x42
+
+	// sta 0x6000
+	rom[2] = 0x8d
+	rom[3] = 0x00
+	rom[4] = 0x60
 
 	err := ioutil.WriteFile("rom.bin", rom, 0644)
 
